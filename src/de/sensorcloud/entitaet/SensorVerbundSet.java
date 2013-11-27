743c7ee8010171cf031f4d3d61f96c47fcbc789d
Arthur Schneider <nc-schneiar15@netcologne.de>
2013-11-27 20:53:11 +0100

    update

---
+++ src/de/sensorcloud/entitaet/SensorVerbundSet.java
@@ -0,0 +1,17 @@
+package de.sensorcloud.entitaet;
+
+import java.util.HashSet;
+
+public class SensorVerbundSet {
+	
+	private HashSet<SensorVerbund> senVerbundSet;
+
+	public HashSet<SensorVerbund> getSenVerbundSet() {
+		return senVerbundSet;
+	}
+
+	public void setSenVerbundSet(HashSet<SensorVerbund> senVerbundSet) {
+		this.senVerbundSet = senVerbundSet;
+	}
+
+}
